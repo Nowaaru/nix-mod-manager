61174c9ac0cd8ad5bf315caab8540d3263efee0b
Nowaaru <nowaaru@proton.me>
2024-03-12 16:41:56 -0700

    *try* and implement nixt
    is it even real..?

--- flake.nix
+++ flake.nix
@@ -4,17 +4,43 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     noire-utils.url = "path:/home/noire/Documents/nix-flakes/noire-utils";
+
+    nixt = {
+      url = "github:nix-community/nixt";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
-    self,
     nixpkgs,
     noire-utils,
-  }: {
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    #
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    nixt,
+    ...
+  } @ inputs: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    __nixt = nixt.lib.grow {
+      blocks = [
+        nixt.lib.block'
+        ./flake.nix
+        {
+          "nixt"."passes this test" = true;
+          "nixt"."fails this test" = false;
+        }
+      ];
+    };
 
-    nixosModules.default = import ./. { inherit nixpkgs noire-utils; };
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [nixt.packages.x86_64-linux.default];
+
+      inputsFrom = [];
+
+      shellHook = ''
+        export DEBUG=1
+      '';
+    };
+    nixosModules.default = import ./. {inherit pkgs;};
   };
 }
