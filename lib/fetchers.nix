c1037ee590f745d405b65ff1a59b7601a4f8ad41
Nowaaru <nowaaru@proton.me>
2024-03-12 16:41:18 -0700

    implement gamebanana fetcher

--- lib/fetchers.nix
+++ lib/fetchers.nix
@@ -1,11 +1,18 @@
 {lib}: let
-  inherit (lib) noire;
-
-  GameBananaMod = with lib.types;
-    mkOptionType {
-      name = "game-banana-mod";
-      check = modData: {};
-    };
+  /*
+     TODO: replace type check with
+  a dynamic alternative that takes in an example
+  (noire-utils)
+  */
+  areListValues = qualifier: with lib.lists; all qualifier;
+  valuesExist = keys:
+    with lib.lists; all (i: builtins.hasAttr keys);
 in {
-  fetchGameBanana = traits: {};
+  fetchGameBanana = traits:
+    with lib;
+      lib.mkIf (asserts.assertMsg (valuesExist ["modId" "downloadId"] traits) "all '...Id' fields are required")
+      lib.mkIf (asserts.assertMsg (areListValues builtins.isInt) [traits.modId traits.downloadId] "all '...Id' fields need to be an integer")
+      fetchurl {
+        url = "https://gamebanana.com/mods/download/${traits.modId}#FileInfo_${traits.downloadId}";
+      };
 }
