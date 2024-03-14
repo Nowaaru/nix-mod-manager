b61e4118fe44a46ae01bca35a196d7a11a6761c3
Nowaaru <nowaaru@proton.me>
2024-03-13 22:35:58 -0700

    add home manager, fix remove nixt, devshell

--- flake.nix
+++ flake.nix
@@ -1,46 +1,28 @@
 {
-  description = "Nix Mod Manager";
+  description = "Noire's Nix Mod Manager";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     noire-utils.url = "path:/home/noire/Documents/nix-flakes/noire-utils";
-
-    nixt = {
-      url = "github:nix-community/nixt";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    nixpkgs,
-    noire-utils,
-    nixt,
-    ...
-  } @ inputs: let
+  outputs = {nixpkgs, home-manager, ...} @ inputs: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
   in {
-    __nixt = nixt.lib.grow {
-      blocks = [
-        nixt.lib.block'
-        ./flake.nix
-        {
-          "nixt"."passes this test" = true;
-          "nixt"."fails this test" = false;
-        }
-      ];
-    };
-
     devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [nixt.packages.x86_64-linux.default];
-
       inputsFrom = [];
 
       shellHook = ''
         export DEBUG=1
       '';
     };
-    nixosModules.default = import ./. {inherit pkgs;};
+
+    nixosModules.default = {config, ...}: import ./. {inherit pkgs home-manager config;};
   };
 }
