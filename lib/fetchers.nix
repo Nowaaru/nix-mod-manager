994d1e4bf1e3342577580da369950e8918f7ef6b
Nowaaru <nowaaru@proton.me>
2024-03-14 19:29:56 -0700

    fmt

--- lib/fetchers.nix
+++ lib/fetchers.nix
@@ -9,17 +9,39 @@
   valuesExist = keys:
     with lib.lists; all (i: builtins.hasAttr keys);
 in {
-  fetchGameBanana = { downloadId, ... }:
+  /*
+  TODO: make a fetchMod function that uses
+  the `pkgs.file` package to check the file
+  type and decompress accordingly.
+  */
+
+  /*
+  TODO: have fetchGameBanana utilize the fetchMod
+  function
+  */
+  fetchMod = {
+    name ? "mod",
+    url,
+    hash ? lib.fakeHash,
+  } @ mod-data: derivation 
+  {
+    inherit name;
+    system = builtins.currentSystem;
+
+    outputs = [ "out" ];
+  };
+
+  fetchGameBanana = {downloadId, ...}:
     with lib;
-      if
-        true
-        # (asserts.assertMsg (valuesExist ["modId" "downloadId"] traits) "all '...Id' fields are required") &&
-        # (asserts.assertMsg (areListValues builtins.isInt) [traits.modId traits.downloadId] "all '...Id' fields need to be an integer")
+      if true
+      # (asserts.assertMsg (valuesExist ["modId" "downloadId"] traits) "all '...Id' fields are required") &&
+      # (asserts.assertMsg (areListValues builtins.isInt) [traits.modId traits.downloadId] "all '...Id' fields need to be an integer")
       then
         pkgs.fetchurl {
           # url = "https://gamebanana.com/mods/download/${builtins.toString traits.modId}#FileInfo_${builtins.toString traits.downloadId}";
           url = "https://gamebanana.com/dl/${builtins.toString downloadId}";
           hash = "sha256-EE5UgIX11w3uenbENh26LDTMQP5uOgTGQkbDMZFE8eo=";
+          curlOptsList = ["-O"];
         }
       else {};
 }
