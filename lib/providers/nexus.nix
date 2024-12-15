004b30d404349df20d302145529532e56aace633
Nowaaru <nowaaru@proton.me>
2024-12-15 12:31:18 -0700

    update nexus provider to use new provider implementation
    yea

--- lib/providers/nexus.nix
+++ lib/providers/nexus.nix
@@ -1,50 +1,20 @@
-pkgs: let
-  mkLocalMod = import ../mklocalmod.nix pkgs;
-  inherit (pkgs) lib;
-
+lib:
+with lib.nnmm; let
   stringify = builtins.toString;
-in
-  with pkgs; rec {
-    mkApiPath = protocol: host: "${protocol}://${host}";
+in {
+  mkApiPath = protocol: host: "${protocol}://${host}";
 
-    mkProvider = provider:
-      provider
-      // {
-        useGame = game-id:
-          lib.attrsets.mapAttrs (_: v: v game-id) provider;
+  mkNexus = providers.generic.mkProvider ({unpackSingularFolders ? false} @ layeredProviderArguments: {
+    fetchNexus = game-domain-name: {
+      name ? "nexus-mod-${stringify mod-id}-${stringify file-id}-${stringify hash}",
+      hash ? lib.fakeHash,
+      hash-type ? "md5",
+      mod-id,
+      file-id,
+    }:
+      mkRequest {
+        inherit hash hash-type name;
+        endpoint = "v1/games/${stringify game-domain-name}/mods/${stringify mod-id}/files/${stringify file-id}/download_link.json";
       };
-
-    mkNexus = api-key: let
-      api-base-uri = mkApiPath "https" "api.nexusmods.com";
-    in
-      mkProvider rec {
-        mkRequest = api:
-          builtins.fromJSON (builtins.readFile (fetchurl {
-            name = "request-nexus";
-            url = "${api-base-uri}/${api}";
-
-            downloadToTemp = true;
-            curlOptsList = [
-              ''-H "accept: application/json"''
-              ''-H "apikey: ${api-key}"''
-            ];
-
-            inherit hash;
-          }));
-
-        fetchNexus = game-domain-name: {
-          name ? "nexus-mod-${stringify mod-id}-${stringify file-id}-${stringify hash}",
-          hash ? lib.fakeHash,
-          mod-id,
-          file-id,
-        }:
-          mkLocalMod {
-            inherit name;
-
-            store-path = stdenv.mkDerivation {
-              name = "request-${name}";
-              src = mkRequest "v1/games/${stringify game-domain-name}/mods/${stringify mod-id}/files/${stringify file-id}/download_link.json";
-            };
-          };
-      };
-  }
+  });
+}
