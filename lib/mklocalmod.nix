13ade6dad58eb065fad74b0c3c4074d476fa4afe
Nowaaru <nowaaru@proton.me>
2024-12-15 12:13:22 -0700

    change mkLocalMod implementation
    also add a new passthru option to unpack singular folders
    might move this to a client definition? idk

--- lib/mklocalmod.nix
+++ lib/mklocalmod.nix
@@ -1,14 +1,20 @@
-pkgs: {
+stdenv: {
   name ? "mod",
   store-path,
+  # Whether to automatically
+  # unpack mods that have one
+  # entry in their top-level
+  unpackSingularFolders ? false,
   unpackPhase ? ''true'',
 }:
-with pkgs;
-  stdenv.mkDerivation {
-    inherit name;
-    src = store-path;
+stdenv.mkDerivation {
+  inherit name;
+  src = store-path;
 
-    phases = ["installPhase"];
-    installPhase = ''cp $src $out'';
-    passthru.unpackPhase = unpackPhase;
-  }
+  phases = ["installPhase"];
+  installPhase = ''ln -sv $src $out'';
+
+  passthru = {
+    inherit unpackPhase unpackSingularFolders;
+  };
+}
