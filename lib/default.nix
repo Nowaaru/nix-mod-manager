4118ce728420b2d31dccdebe945157a538bff92a
Nowaaru <nowaaru@proton.me>
2024-12-15 12:08:20 -0700

    change recursiveApply impl

--- lib/default.nix
+++ lib/default.nix
@@ -1,14 +1,10 @@
-{
-  pkgs,
-  lib,
-}: let
-  st = w: builtins.trace w w;
+pkgs: let
+  inherit (pkgs) lib;
   recursiveApply = with lib.attrsets;
     what: to:
-      foldlAttrs (acc: k: v: acc // lib.attrsets.setAttrByPath [(st k)] ((st v) what)) {} to;
+      foldlAttrs (acc: k: v: acc // (lib.attrsets.setAttrByPath [k] (v what))) {} to;
 in
-  recursiveApply pkgs {
+  recursiveApply lib {
     providers = import ./providers;
     fetchers = import ./fetchers.nix;
-    mkLocalMod = import ./mklocalmod.nix;
   }
