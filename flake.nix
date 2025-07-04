a2d669aa15b7832c7a53d7bb4da5c47a80d1c534
Nowaaru <nowaaru@proton.me>
2025-07-04 03:53:57 -0700

    remove `rust-overlay` from inputs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-    };
+    home-manager.url = "github:nix-community/home-manager/master";
   };
 
   outputs = {
